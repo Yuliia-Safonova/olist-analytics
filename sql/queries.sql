acf0ac63667c8977bedc65fa39e7c07870cf7360
Yuliia_Safonova <yuliya.safo@gmail.com>
2026-07-07 15:36:17 +0200

    Add varios SQL

--- sql/queries.sql
+++ sql/queries.sql
@@ -28,3 +28,59 @@ JOIN olist_order_items_dataset oi USING (order_id)
 WHERE o.order_status = 'delivered'
 GROUP BY ym
 ORDER BY ym;
+
+-- ------------------------------------------------------
+
+-- Розвідувальні запити
+-- ТОП-10 категорій за виторгом
+SELECT
+	t.product_category_1 AS category_en,
+	ROUND(SUM(oi.price), 2) AS revenue
+FROM olist_order_items_dataset oi
+JOIN olist_orders_dataset o USING (order_id)
+JOIN olist_products_dataset p USING (product_id)
+LEFT JOIN product_category_name_translation t USING (product_category)
+WHERE o.order_status = 'delivered'
+GROUP BY category_en
+ORDER BY revenue DESC
+LIMIT 10;
+
+-- Виторг за штатами (для карти в Tableau)
+SELECT
+	cu.customer_state,
+    ROUND(SUM(oi.price), 2) AS revenue,
+    COUNT(DISTINCT o.order_id) AS orders
+FROM olist_order_items_dataset oi
+JOIN olist_orders_dataset o USING (order_id)
+JOIN olist_customers_dataset cu USING (customer_id)
+WHERE o.order_status = 'delivered'
+GROUP BY cu.customer_state
+ORDER BY revenue DESC;
+
+-- Середня оцінка (review_score) за категоріями
+SELECT
+	t.product_category_1 AS category_en,
+    ROUND(AVG(r.review_score), 2) AS avg_score,
+    COUNT(*) AS reviews
+FROM olist_order_reviews_dataset r
+JOIN olist_order_items_dataset oi USING (order_id)
+JOIN olist_products_dataset p USING (product_id)
+LEFT JOIN product_category_name_translation t USING (product_category)
+GROUP BY category_en
+HAVING reviews > 50
+ORDER BY avg_score DESC;
+
+-- Середній час доставки (різниця між датою купівлі і датою доставки)
+SELECT
+	ROUND(AVG(julianday(order_delivered_6) - julianday(order_purchase_t)), 1) AS avg_delivery_days
+FROM olist_orders_dataset
+WHERE order_status = 'delivered' AND order_delivered_6 IS NOT NULL;
+
+-- Розподіл способів оплати
+SELECT
+	payment_type,
+    COUNT(*) AS n,
+    ROUND(SUM(payment_value), 2) AS total_value
+FROM olist_order_payments_dataset
+GROUP BY payment_type
+ORDER BY n DESC;
